439182cee7be6808494fbb5c4eabafcf231e3dc2
Wang Lifeng <lifeng.a.wang@intel.com>
2025-04-17 10:55:10 +0800

    Merge pull request #1 from LifengWang/lifeng_dev
    add two RAG implementations based on LangChain and OPEA

---
+++ RAG_opea/set_env.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+export host_ip=10.112.100.76
+export HUGGINGFACEHUB_API_TOKEN="your_huggingface_api_token"
+export no_proxy=127.0.0.1,localhost,chatqna-xeon-ui-server,chatqna-xeon-backend-server,dataprep-redis-service,tei-embedding-service,retriever,tei-reranking-service,tgi-service,vllm-service,llm-faqgen
+
+export EMBEDDING_MODEL_ID="BAAI/bge-base-en-v1.5"
+export RERANK_MODEL_ID="BAAI/bge-reranker-base"
+export LLM_MODEL_ID="deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+export INDEX_NAME="rag-redis"
+# Set it as a non-null string, such as true, if you want to enable logging facility,
+# otherwise, keep it as "" to disable it.
+export LOGFLAG=""
+# Set OpenTelemetry Tracing Endpoint
+export JAEGER_IP=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+')
+export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=grpc://$JAEGER_IP:4317
+export TELEMETRY_ENDPOINT=http://$JAEGER_IP:4318/v1/traces
+# Set no proxy
+export no_proxy="$no_proxy,chatqna-xeon-ui-server,chatqna-xeon-backend-server,dataprep-redis-service,tei-embedding-service,retriever,tei-reranking-service,tgi-service,vllm-service,jaeger,prometheus,grafana,node-exporter,$JAEGER_IP"
+
+export LLM_ENDPOINT_PORT=8010
+export LLM_SERVER_PORT=9009
+export CHATQNA_BACKEND_PORT=8888
+export CHATQNA_REDIS_VECTOR_PORT=6379
+export CHATQNA_REDIS_VECTOR_INSIGHT_PORT=8001
+export CHATQNA_FRONTEND_SERVICE_PORT=5173
+export NGINX_PORT=80
+export FAQGen_COMPONENT_NAME="OpeaFaqGenvLLM"
+export LLM_ENDPOINT="http://${host_ip}:${LLM_ENDPOINT_PORT}"
